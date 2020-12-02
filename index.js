a1a7b9cfcbb0d3dd6289e72ee6b3b391abd26375
costarafaelhugo <costarafaelhugo@gmail.com>
2020-12-02 17:34:55 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+document.getElementById("countButton").onclick = function () {
+   
+   
+    let typedText = document.getElementById("textInput").value;
+    typedText = typedText.toLowerCase();
+    typedText = typedText.replace(/[^a-z'\s]+/g, "");
+    const letterCounts = {};
+
+
+    for (let i = 0; i < typedText.length; i++) {
+        currentLetter = typedText[i];
+             if (letterCounts[currentLetter] === undefined) {
+            letterCounts[currentLetter] = 1;
+        } else {
+            letterCounts[currentLetter]++;
+        }
+    }
+
+
+    var div1 = document.getElementById('lettersDiv')
+
+    for (letters in letterCounts) {
+        var lettersSpan = document.createElement('span')
+        lettersSpan = document.createTextNode(letters + " : " + letterCounts[letters] + ' , ')
+        div1.appendChild(lettersSpan)
+    }
+
+    words = typedText.split(/\s/);
+
+    const countWords = {
+
+    };
+
+
+    for (let i = 0; i < words.length; i++) {
+        currentWord = words[i];
+
+
+
+
+        if (countWords[currentWord] === undefined) {
+            countWords[currentWord] = 1;
+        } else {
+            countWords[currentWord]++;
+        }
+
+    }
+    var div2 = document.getElementById('wordsDiv')
+
+    for (words in countWords) {
+        var wordsSpan = document.createElement('span')
+        wordsSpan= document.createTextNode(words + ": " + countWords[words] + ', ')
+        div2.appendChild(wordsSpan)
+    }
+}
+
